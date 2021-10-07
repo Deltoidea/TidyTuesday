01b53b05b2648810f1c5e23d896583be9962996f
Cody <user@pop-os.localdomain>
2021-10-07 11:31:59 -0600

    TidyTuesday update

--- TidyTuesday/2021-10-05/2021-10-05.R
+++ TidyTuesday/2021-10-05/2021-10-05.R
@@ -1,45 +1,57 @@
 library(tidyverse)
 library(tidytuesdayR)
 
+# 
+# df <- tt_load("2021-10-05")
+# 
+# df <- df$nurses
+# 
+# regions <- read_csv(url("https://raw.githubusercontent.com/cphalpert/census-regions/master/us%20census%20bureau%20regions%20and%20divisions.csv")) %>% 
+#   select(State, Region, Division)
+# 
+# write_csv(df, file = "./Nurses_with_region.csv")
 
-df <- tt_load("2021-10-05")
+df <- read_csv("Nurses_with_region.csv")
+#Get inflation rate per year.
 
+inflation_rate <- read_csv("./united-states-inflation-rate.csv", skip = 16) %>%
+  mutate(Year = date %>% lubridate::year())
+inflation_rate$inf_rate <- inflation_rate$`Inflation Rate (%)`/100 
+inf_rate <- inflation_rate %>% 
+  select(Year, inf_rate) %>% 
+  filter(Year %in% c(df$Year %>%unique)) %>% 
+  mutate(inf_rate = cumsum(x = inf_rate)+1)
 
-df <- df$nurses
-
-
-df %>% names
-
-df %>% pivot_longer(cols = starts_with("Annual"), 
-                    names_to = "Salary", 
-                    values_to = "valueS") %>% 
-  pivot_longer(cols = starts_with("Hourly"), 
-               names_to = "Hourly", 
-               values_to = "valueH")
-
-regions <- read_csv(url("https://raw.githubusercontent.com/cphalpert/census-regions/master/us%20census%20bureau%20regions%20and%20divisions.csv")) %>% 
-  select(State, Region, Division)
-
-df %>% names
-
-df <- df %>% pivot_longer(cols = starts_with("Annual"), 
-                          names_to = "Salary", 
-                          values_to = "valueS") %>% 
-  pivot_longer(cols = starts_with("Hourly"), 
-               names_to = "Hourly", 
-               values_to = "valueH") 
-df %>% left_join(., regions)
-df
 df %>% 
-  group_by(State, Year) %>% 
-  summarise(mean = mean(`Hourly Wage Avg`)) %>% 
+  filter(Salary == "Annual Salary Avg") %>% 
+  select(State, Year, valueS, Region) %>% 
+  left_join(.,inf_rate) %>% 
+  arrange(Year, State) %>%
+  group_by(State) %>% 
+  mutate(`Adjusted Salary` = valueS/(inf_rate)) %>%
+  ungroup() %>% 
+  distinct() %>% 
+  pivot_longer(cols = c(valueS, `Adjusted Salary`)) %>% 
+  group_by(Year, Region, name) %>% 
+  summarise(value = mean(value)) %>% 
+  na.omit() %>% 
+  ggplot(aes(x = Year,  color = Region)) +
+  geom_line(aes(y = value)) +
+  facet_wrap(~name)
   
-  ggplot(aes(x = Year, y = mean, group = State)) + 
+df$Salary %>% unique()
+df %>% 
+  ggplot(aes(x = Year, y = valueS, group = State)) + 
   geom_line(stat = "identity", alpha = 0.5) +
-  geom_line(data = df %>% 
-              group_by(State, Year) %>% 
-              summarise(mean = mean(`Hourly Wage Avg`)) %>% 
-              filter(State == "Utah"), aes(color = State)) +
-  labs( y = " Mean Hourly Wage", title = "") +
+  geom_line(data = df %>%  
+              filter(State %in% c("Utah", "California", "Washington", "Louisiana")), aes(color = State)) +
+  labs( y = "", title = "", x = "") +
   theme_classic() +
-theme(legend.title = element_blank()) 
+theme(legend.po = "none",
+      axis.text = element_blank(),
+      axis.line = element_blank(),
+      axis.ticks = element_blank(),  ) + coord_polar()
+
+
+# https://www.macrotrends.net/countries/USA/united-states/inflation-rate-cpi
+
